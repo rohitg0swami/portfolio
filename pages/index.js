2ee31135fb326f8b1b5644b2d9f735e37e422214
Rohit Goswami <rohitgoswamigi@gmail.com>
2025-01-14 14:43:38 +0530

    Add Some meta tags for SEO

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,14 @@ export default function Home() {
         <div>
             <Head>
                 <title>Rohit Goswami - Software Engineer</title>
+                <meta
+                    name="description"
+                    content="Rohit Goswami, a highly skilled software engineer specializing in web development, backend systems, and scalable solutions. Discover insights, projects, and expertise in building modern applications."
+                />
+                <meta
+                    name="keywords"
+                    content="Rohit Goswami, software engineer, web developer, backend developer, scalable solutions, modern applications, programming, coding, software development, technology, full-stack engineer, cloud solutions, API development"
+                />
             </Head>
             <HomePage />
         </div>
