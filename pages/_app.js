fc4217d9ddbc7b22a622fa0d6812c284b862818b
Rohit Goswami <rohitgoswamigi@gmail.com>
2025-01-19 18:17:15 +0530

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,28 @@ import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 import { DefaultSeo } from "next-seo"; // Import DefaultSeo from next-seo
 import defaultSEO from "../next-seo.config";
+import Script from "next/script"; // Import the Script component
 
 function MyApp({ Component, pageProps }) {
     return (
         <div className="bg-white">
+            {/* Google Analytics Script */}
+            <Script
+                strategy="afterInteractive"
+                src="https://www.googletagmanager.com/gtag/js?id=G-0CXL37MGEY"
+            />
+            <Script
+                id="google-analytics"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0CXL37MGEY');
+          `,
+                }}
+            />
             <DefaultSeo {...defaultSEO} />
             <Navbar />
             <Component {...pageProps} />
