640eb853fcde16f9806cc30bc0e737798b457f27
Rohit Goswami <rohitgoswamigi@gmail.com>
2025-04-03 08:44:02 +0530

    small change

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div>
             <NextSeo
-                title="Rohit Goswami - Software Engineer"
+                title="Rohit Goswami - Software Engineer | Kaithal, India"
                 description="Rohit Goswami, a highly skilled software engineer specializing in web development, backend systems, and scalable solutions. Discover insights, projects, and expertise in building modern applications."
                 keywords="Rohit Goswami, software engineer, software engineer in Kaithal, web developer, backend developer, scalable solutions, modern applications, programming, coding, software development, technology, full-stack engineer, cloud solutions, API development"
                 openGraph={{
